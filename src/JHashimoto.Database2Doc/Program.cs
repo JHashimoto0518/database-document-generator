22f853307e513845bebc6f741fd534ae26c3e368
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2021-01-09 16:23:03 +0900

    impl query from database

--- src/JHashimoto.Database2Doc/Program.cs
+++ src/JHashimoto.Database2Doc/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace JHashimoto.Database2Doc {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            using (var con = new SqlConnection("Server=(local);Database=master;User Id=sa;Password=t6Eww7lL4teE;")) {
+                con.Open();
+
+                var rows = con.Query("select name from sys.databases").AsList();
+                Console.WriteLine(rows[0].name);
+            }
+            
         }
     }
 }
