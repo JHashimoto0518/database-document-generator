f4d8255705986edf760385255b74d600587dced3
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2021-01-11 16:07:58 +0900

    add AllowNullAttribute to properties

--- src/JHashimoto.Database2Doc/Schema/Models/Table.cs
+++ src/JHashimoto.Database2Doc/Schema/Models/Table.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JHashimoto.Database2Doc.Schema.Models {
     
-    // TODO:エラーメッセージ日本語化
-    public record Table {
+    public record Table() {
+        [AllowNull]
         public string Database { get; init; }
+        [AllowNull]
         public string Schema { get; init; }
+        [AllowNull]
         public string TableName { get; init; }
+        [AllowNull]
         public string TableType { get; init; }
     }
 }
