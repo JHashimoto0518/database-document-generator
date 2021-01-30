93fcc66b68b2f8eca7f1aef8ffc8ac3c25b0371f
Junichi Hashimoto <junichi_hashimoto@hotmail.co.jp>
2021-01-30 21:12:27 +0900

    impl DbProviderTypes

--- src/JHashimoto.Repositories.Database/DbProviderType.cs
+++ src/JHashimoto.Repositories.Database/DbProviderType.cs
@@ -7,23 +7,18 @@ using System.Text;
 
 namespace JHashimoto.Repositories.Database {
     public enum DbProviderTypes {
-        [Description("Microsoft.Data.SqlClient")]
+        [DbProviderInvariantName("Microsoft.Data.SqlClient")]
         SqlServer = 1
     }
 
     public static class DbProviderTypesExtensions {
         public static string GetProperName<T>(this T Value) where T : Enum {
-            if (!(typeof(T).IsEnum)) {
-                throw new InvalidEnumArgumentException();
-            }
-
             FieldInfo fieldInfo = Value.GetType().GetField(Value.ToString());
-            if (fieldInfo == null) return null;
 
-            var attr = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<DescriptionAttribute>().FirstOrDefault();
+            var attr = fieldInfo?.GetCustomAttributes(typeof(DbProviderInvariantNameAttribute), false).Cast<DbProviderInvariantNameAttribute>().FirstOrDefault()
+                        ?? throw new Exception("指定された文字列に合致するEnumメンバーはありません。");
 
-            if (attr == null) throw new Exception();
-            return attr.Description;
+            return attr?.InvariantName ?? throw new Exception($"指定されたEnumメンバーに{nameof(DbProviderInvariantNameAttribute)}がありません。");
         }
     }
 }
